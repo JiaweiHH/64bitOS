976fe7fc4668c3363599a40cfc92b5616dce2b68
jiawei <lixpoo@163.com>
2021-07-27 21:26:19 +0800

    异常处理函数补全

--- src/include/trap.h
+++ src/include/trap.h
@@ -6,25 +6,25 @@
 #include "printk.h"
 
 void divide_error();
-// void debug();
-// void nmi();
-// void int3();
-// void overflow();
-// void bounds();
-// void undefined_opcode();
-// void dev_not_available();
-// void double_fault();
-// void coprocessor_segment_overrun();
+void debug();
+void nmi();
+void int3();
+void overflow();
+void bounds();
+void undefined_opcode();
+void dev_not_available();
+void double_fault();
+void coprocessor_segment_overrun();
 void invalid_TSS();
-// void segment_not_present();
-// void stack_segment_fault();
-// void general_protection();
+void segment_not_present();
+void stack_segment_fault();
+void general_protection();
 void page_fault();
-// void x87_FPU_error();
-// void alignment_check();
-// void machine_check();
-// void SIMD_exception();
-// void virtualization_exception();
+void x87_FPU_error();
+void alignment_check();
+void machine_check();
+void SIMD_exception();
+void virtualization_exception();
 
 void sys_vector_init();
 
